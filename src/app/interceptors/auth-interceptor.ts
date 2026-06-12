f492951532115ea3db1f987c198b7c8c18c7eed5
zcodlab <zmamanir@uni.edu.pe>
2026-06-12 10:32:25 -0500

    modify auth-interceptor

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -18,22 +18,25 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   ];
 
   const shouldExclude = excludedPaths.some(path =>
+    //verifica si req contiene alguna de la rutas que no requieren token
     req.url.includes(path)
   );
-
+  //si req incluye una ruta de la lista de rutas excluidas
   if (shouldExclude) {
+    //continua con la ejecucion del request
     return next(req);
   }
 
+  //recupera el token
   const token = storageService.getToken();
 
   // Verificacion proactiva: si el token ha caducado, actualicelo antes de enviar la solicitud.
   if (token && storageService.isTokenExpired(token)) {
     return refreshTokenAndRetry(req, next, authService);
   }
-
+  //declara un nuevo objeto request, debido a que req es inmutable
   let authReq = req;
-
+  //si se cuenta con token vigente, clona el request para incorporar el token en el header de la peticion http
   if (token) {
     authReq = req.clone({
       setHeaders: {
@@ -41,7 +44,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       }
     });
   }
-
+  //continua con la ejecucion http que fue interceptada y captura la respuesta
   return next(authReq).pipe(
     catchError((error) => {
       // Opcion alternativa: si obtenemos un error 403,
@@ -62,6 +65,7 @@ const refreshTokenAndRetry = (request: any, next: any, authService: AuthService)
     return authService.refreshToken().pipe(
       switchMap((response) => {
         isRefreshing = false;
+        //comunica a sus suscriptores del nuevo token
         refreshTokenSubject.next(response.token);
         return next(request.clone({
           setHeaders: {
@@ -78,7 +82,7 @@ const refreshTokenAndRetry = (request: any, next: any, authService: AuthService)
   } else {
     return refreshTokenSubject.pipe(
       filter(token => token !== null),
-      take(1),
+      take(1),  //toma el primer token y luego se desuscribe
       switchMap((token) => {
         return next(request.clone({
           setHeaders: {
